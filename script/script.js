8aecce39ec9e12e4f0f31287612a0b951391c502
jangmi <wkdal9748@naver.com>
2023-09-01 18:20:13 +0900

    한화

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const main2info = document.querySelectorAll('.swiper-slide')
+console.log(main2info)
+for(let i of main2info){i.style.display = 'none'} //전체통합해서 지우기
+main2info[0].style.display = 'flex'
+// main2info[0].style.display = 'none'
+// main2info[1].style.display = 'none'
+// main2info[2].style.display = 'none'
