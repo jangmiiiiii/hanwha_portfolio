c38972567b8b508fd56221d443a583d0c695875f
jangmi <wkdal9748@naver.com>
2023-09-22 18:57:55 +0900

    hanwha portfolio

--- script/jquery.fullPage.js
+++ script/jquery.fullPage.js
@@ -1,15 +1,9 @@
-/*!
- * fullPage 2.9.7
- * https://github.com/alvarotrigo/fullPage.js
- * @license MIT licensed
- *
- * Copyright (C) 2015 alvarotrigo.com - A project by Alvaro Trigo
- */
+
 (function(global, factory) {
     'use strict';
     if (typeof define === 'function' && define.amd) {
         define(['jquery'], function($) {
-          return factory($, global, global.document, global.Math);
+        return factory($, global, global.document, global.Math);
         });
     } else if (typeof exports === "object" && exports) {
         module.exports = factory(require('jquery'), global, global.document, global.Math);
